f8469348f9df4c990a93b1f833be4ccaaf07b692
Laura Botero <bopelasco@gmail.com>
2018-11-12 17:28:15 -0500

    no message

---
+++ LauraBotero-Platformer2D/Assets/Scripts/MovieController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MovieController : MonoBehaviour {
+
+    private VideoPlayer m_VideoPlayer;
+
+	// Use this for initialization
+    void Awake () {
+
+        // Cargamos el componente VideoPlayer
+        m_VideoPlayer = GetComponent<VideoPlayer>();
+
+        // loopPointReached es el evento para el fin del video
+        m_VideoPlayer.loopPointReached += OnMovieFinished;
+	}
+
+    void OnMovieFinished(VideoPlayer player)
+    {
+        Debug.Log("Event for movie end called");
+        player.Stop();
+
+        // Cargan el prefab del nivel 1
+        GameObject newLevel = GameObject.Instantiate(Resources.Load("Levels/Level1") as GameObject);
+
+    }
+	    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
